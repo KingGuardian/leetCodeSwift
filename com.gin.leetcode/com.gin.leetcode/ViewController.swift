67038b1cb8c81ebcb9727e88962d087d2f0b00e6
穆秋实 <gintamatama0912@gmail.com>
2018-12-29 10:53:32 +0800

    初始提交

---
+++ com.gin.leetcode/com.gin.leetcode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  com.gin.leetcode
+//
+//  Created by 穆秋实 on 2018/12/29.
+//  Copyright © 2018 穆秋实. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
